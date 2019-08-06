2a0ebccbfd63c804bc6dfad84c98a3136b2666e6
Magno Moreira <magnomoreira@yahoo.com>
2019-08-06 17:41:19 -0300

    Criando conexão com o banco de dados MongoDb

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const routes = express.Router();
+const DevController = require ('./controllers/DevController');
+const LikeController = require ('./controllers/likeController');
+const DeslikeController = require ('./controllers/DeslikeController')
 
-routes.get('/', (req, res) => {
-    return res.json({ message: `hello ${req.query.name}` });
-});
 
-routes.post('/devs',(req,res)=>{
-    return res.json(req.body);
-});
+routes.get ('/devs', )
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devid/likes', LikeController.store);
+routes.post('/devs/:devid/Deslikes', DeslikeController.store);
 module.exports= routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
+
 const server = express();
 
+mongoose.connect('mongodb://omnistack:react1@ds060478.mlab.com:60478/base_mongo',{ useNewUrlParser:true })
 server.use(express.json());
+server.user(cors());
 server.listen(3333);
 server.use(routes);
